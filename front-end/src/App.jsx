88675602cf016517494ed0644cfc64a385426287
samuelalbuquerque01 <samuel_albuquerque_f@hotmail.com>
2025-09-16 09:14:36 -0300

    WIP: alterações locais antes do rebase

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,13 @@
+// App.jsx
 import './App.css'
+import AppointmentsScreen from './AppointmentsScreen/AppointmentsScreen.jsx'
 
 function App() {
-
+  return (
+    <div className="App">
+      <AppointmentsScreen />
+    </div>
+  )
 }
 
 export default App
